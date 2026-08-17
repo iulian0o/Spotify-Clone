0edddf9a1c396bcb7ea32ac527cb71120d83f4a9
Iulian Asanache <costi-iulian.asanache@epita.fr>
2026-08-17 15:51:58 +0200

    First commit

---
+++ frontend/src/App.tsx
@@ -0,0 +1,24 @@
+import { Show, SignInButton, SignUpButton, UserButton } from "@clerk/react";
+import { Button } from './components/ui/button'
+
+function App() {
+  return (
+    <>
+      <header>
+        <Show when="signed-out">
+          <SignInButton>
+            <Button>Sign In</Button>
+          </SignInButton>
+          <SignUpButton>
+            <Button>Sign Up</Button>
+          </SignUpButton>
+        </Show>
+        <Show when="signed-in">
+          <UserButton />
+        </Show>
+      </header>
+    </>
+  );
+}
+
+export default App;
