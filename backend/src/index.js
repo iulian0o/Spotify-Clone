22038a386e0a7ed4a9772d5e9436c25f30f05bb5
Iulian Asanache <costi-iulian.asanache@epita.fr>
2026-08-19 09:56:27 +0200

    fix: implement cors to talk to frontend

--- backend/src/index.js
+++ backend/src/index.js
@@ -2,6 +2,7 @@ import express from 'express';
 import dotenv from 'dotenv';
 import fileUpload from 'express-fileupload';
 import path from 'path';
+import cors from 'cors';
 
 import { connectDB } from './lib/db.js';
 import { clerkMiddleware } from '@clerk/express';
@@ -18,6 +19,13 @@ dotenv.config();
 const __dirname = path.resolve();
 const app = express();
 const PORT = process.env.PORT;
+
+app.use(cors(
+  {
+    origin: 'http://localhost:3000',
+    credentials: true
+  }
+))
 
 app.use(express.json());
 app.use(clerkMiddleware());
