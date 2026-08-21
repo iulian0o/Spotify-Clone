c4c57691769a47d15fb88d4d52ad75c55309ffee
Iulian Asanache <costi-iulian.asanache@epita.fr>
2026-08-21 22:22:59 +0200

    feat: create album page

--- frontend/src/App.tsx
+++ frontend/src/App.tsx
@@ -2,8 +2,9 @@ import { Route, Routes } from 'react-router'
 import { AuthenticateWithRedirectCallback } from '@clerk/react'
 import HomePage from './pages/home/HomePage'
 import AuthCallbackPage from './pages/auth-callback/AuthCallbackPage'
-import MainLayout from './layout/MainLayout';
+import MainLayout from './layout/MainLayout'
 import ChatPage from './pages/chat/ChatPage'
+import AlbumPage from './pages/album/AlbumPage'
 
 function App() {
   return (
@@ -12,9 +13,11 @@ function App() {
         <Route path='/sso-callback' element={<AuthenticateWithRedirectCallback 
           signUpForceRedirectUrl={"/auth-callback"}/>} />
         <Route path='/auth-callback' element={<AuthCallbackPage />} />
+
         <Route element={<MainLayout />}>
           <Route path='/' element={<HomePage />} />
           <Route path='/chat' element={<ChatPage />} />
+          <Route path='/albums/:albumId' element={<AlbumPage />} />
         </Route>
       </Routes>
     </>
