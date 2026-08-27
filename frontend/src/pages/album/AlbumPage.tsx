56c950689f0fb44e185dd759a8ec8a658c562960
Iulian Asanache <costi-iulian.asanache@epita.fr>
2026-08-27 14:50:45 +0200

    fix: fix ScrollArea layout with rounded corners

--- frontend/src/pages/album/AlbumPage.tsx
+++ frontend/src/pages/album/AlbumPage.tsx
@@ -24,7 +24,7 @@ export default function AlbumPage() {
 
   return (
     <div className="h-full">
-      <ScrollArea className="h-full">
+      <ScrollArea className="h-full rounded-md">
         {/* Main Content */}
         <div className="relative min-h-full">
           {/* bg gradient */}
