d2755a798d0db7bc807eb2266ac3448c665330d1
Iulian Asanache <costi-iulian.asanache@epita.fr>
2026-08-29 21:46:49 +0200

    feat: create play button component for song recomandations

--- frontend/src/pages/home/HomePage.tsx
+++ frontend/src/pages/home/HomePage.tsx
@@ -24,19 +24,20 @@ export default function HomePage() {
   }, [fetchFeaturedSongs, fetchMadeForYouSongs, fetchTrendingSongs])
 
   return (
-    <main className="rounded-md overflow-hidden h-full bg-linear-to-b from-zinc-800 to-zinc-900">
-      <TopBar />
-      <ScrollArea className="h-[calc(100vh-180px)]">
-        <div className="p-4 sm:p-6">
-          <h1 className="text-2xl sm:text-3xl font-bold mb-6">Good Afternoon</h1>
-          <FeaturedSection />
-        </div>
-        <div className="space-y-8">
-          <SectionGrid title="Made For Your" songs={madeForYouSongs} isLoading={isLoading} />
-          <SectionGrid title="Trending" songs={trendingSongs} isLoading={isLoading} />
-        </div>
-      </ScrollArea>
-    </main>
+    <main className='rounded-md overflow-hidden h-full bg-gradient-to-b from-zinc-800 to-zinc-900'>
+			<TopBar />
+			<ScrollArea className='h-[calc(100vh-180px)]'>
+				<div className='p-4 sm:p-6'>
+					<h1 className='text-2xl sm:text-3xl font-bold mb-6'>Good afternoon</h1>
+					<FeaturedSection />
+
+					<div className='space-y-8'>
+						<SectionGrid title='Made For You' songs={madeForYouSongs} isLoading={isLoading} />
+						<SectionGrid title='Trending' songs={trendingSongs} isLoading={isLoading} />
+					</div>
+				</div>
+			</ScrollArea>
+		</main>
 
   );
 }
--- frontend/src/pages/home/components/FeaturedSection.tsx
+++ frontend/src/pages/home/components/FeaturedSection.tsx
@@ -1,5 +1,6 @@
 import { useMusicStore } from "../../../stores/useMusicStore.ts"
 import FeaturedGridSkeleton from '../../../components/skeletons/FeaturedGridSkeleton.tsx';
+import PlayButton from './PlayButton';
 
 export default function FeaturedSection() {
   const {isLoading, featuredSongs, error} = useMusicStore();
@@ -20,8 +21,9 @@ export default function FeaturedSection() {
               <p className="font-medium truncate">{song.title}</p>
               <p className="text-sm text-zinc-400 truncate">{song.artist}</p>
             </div>
+            {/* // TODO: add play button */}
+            <PlayButton song={song} />
           </div>
-          // TODO: add play button
       ))}
     </div>
   )
