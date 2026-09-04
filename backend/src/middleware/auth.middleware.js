5954eddcfdcd4e1768f0edad827b86269830e067
Iulian Asanache <costi-iulian.asanache@epita.fr>
2026-09-04 21:25:03 +0200

    fix: destructure the userId from req.auth so it is not treated as true by the condition if not admin

--- backend/src/middleware/auth.middleware.js
+++ backend/src/middleware/auth.middleware.js
@@ -1,7 +1,9 @@
 import { clerkClient } from '@clerk/express';
 
 export const protectRoute = async (req, res, next) => {
-  if (!req.auth.userId) {
+  const { userId } = req.auth();
+
+  if (!userId) {
     return res.status(401).json({ message: "Unauthorized - you must be logged in" });
   }
   next();
@@ -9,7 +11,8 @@ export const protectRoute = async (req, res, next) => {
 
 export const requireAdmin = async (req, res, next) => {
   try {
-    const currentUser = await clerkClient.users.getUser(req.auth.userId);
+    const { userId } = req.auth();
+    const currentUser = await clerkClient.users.getUser(userId);
     const isAdmin = process.env.ADMIN_EMAIL === currentUser.primaryEmailAddress?.emailAddress;
 
     if (!isAdmin) {
