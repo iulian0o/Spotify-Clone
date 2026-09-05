2f23471b55c8bd64f78bb76115aa00b10ed7a707
Iulian Asanache <costi-iulian.asanache@epita.fr>
2026-09-06 01:05:50 +0200

    feat: finish create album in admin page

--- frontend/src/pages/admin/components/AlbumsTabContent.tsx
+++ frontend/src/pages/admin/components/AlbumsTabContent.tsx
@@ -1,28 +1,27 @@
-import { Library } from "lucide-react"
-import { Card, CardHeader, CardTitle, CardDescription, CardContent } from "../../../components/ui/card"
-import { Button } from "../../../components/ui/button"
-import AlbumsTable from "../components/AlbumTable"
+import { Card, CardContent, CardDescription, CardHeader, CardTitle } from "@/components/ui/card";
+import { Library } from "lucide-react";
+import AlbumTable from "./AlbumTable";
+import AddAlbumDialog from "./AddAlbumDialog";
 
 export default function AlbumsTabContent() {
-  return (
-    <Card className="bg-zinc-800/50 border-zinc-700/50">
-      <CardHeader>
-        <div className="flex items-center justify-between">
-          <div>
-            <CardTitle className="flex items-center gap-2">
-              <Library className="h-5 w-5 text-violet-500" />
-              Albums Library
-            </CardTitle>
-            <CardDescription>Manage your album collection</CardDescription>
-          </div>
-        </div>
-        <Button>+</Button>
-      </CardHeader>
+	return (
+		<Card className='bg-zinc-800/50 border-zinc-700/50'>
+			<CardHeader>
+				<div className='flex items-center justify-between'>
+					<div>
+						<CardTitle className='flex items-center gap-2'>
+							<Library className='h-5 w-5 text-violet-500' />
+							Albums Library
+						</CardTitle>
+						<CardDescription>Manage your album collection</CardDescription>
+					</div>
+					<AddAlbumDialog />
+				</div>
+			</CardHeader>
 
-      <CardContent>
-        <AlbumsTable />
-      </CardContent>
-    </Card>
-
-  )
-}
+			<CardContent>
+				<AlbumTable />
+			</CardContent>
+		</Card>
+	);
+};
--- frontend/src/stores/useMusicStore.ts
+++ frontend/src/stores/useMusicStore.ts
@@ -42,23 +42,22 @@ export const useMusicStore = create<MusicStore>((set) => ({
   },
 
   deleteAlbum: async (id) => {
-    set({ isLoading: true, error: null });
-    try {
-      await axiosInstance.delete(`/admin/songs/${id}`)
-
-      set((state) => ({
-        album: state.albums.filter((album) => album._id !== id),
-        songs: state.songs.map((song) => (
-          song.albumId === state.albums.find((a) => a._id === id)?.title ? {...song, album: null} : song
-        )),
-      }));
-      toast.success("Album deleted successfully")
-    } catch (error: any) {
-      toast.error("Error deleting song" + error.message)
-    } finally {
-      set({ isLoading: false })
-    }
-  },
+		set({ isLoading: true, error: null });
+		try {
+			await axiosInstance.delete(`/admin/albums/${id}`);
+			set((state) => ({
+				albums: state.albums.filter((album) => album._id !== id),
+				songs: state.songs.map((song) =>
+					song.albumId === state.albums.find((a) => a._id === id)?.title ? { ...song, album: null } : song
+				),
+			}));
+			toast.success("Album deleted successfully");
+		} catch (error: any) {
+			toast.error("Failed to delete album: " + error.message);
+		} finally {
+			set({ isLoading: false });
+		}
+	},
 
   deleteSong: async (id) => {
     set({ isLoading: true, error: null });
